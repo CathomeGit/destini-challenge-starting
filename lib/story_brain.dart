cdc5951af2a18d5e32a45e494c8fbbb86ebb14e3
CathomeGit <cathome.daily.music@gmail.com>
2020-11-23 20:01:43 +0300

    127. Hide empty button

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -66,6 +66,10 @@ class StoryBrain {
   void restart() {
     _storyPlan.restart();
   }
+
+  bool buttonShouldBeVisible() {
+    return getChoice2().isNotEmpty;
+  }
 }
 
 class _StoryPlan {
@@ -97,7 +101,3 @@ class _Node {
 
   _Node(this.value, this.first, this.second);
 }
-
-//TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
-
-//TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
